0aed38fcfca91963db9b15244a8fd60e4e2e988d
Vamsi-518 <chmnvkrishna@gmail.com>
2026-09-18 19:49:29 +0530

    add

--- app.py
+++ app.py
@@ -3,18 +3,22 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 import mysql.connector
 from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
-app.secret_key = "expense_tracker_secret_key"
+app.secret_key = os.environ.get("SECRET_KEY")
 
 def get_db():
     return mysql.connector.connect(
         host=os.environ.get("DB_HOST"),
-        user=os.environ.get("DB_USER", "root"),
-        password=os.environ.get("DB_PASSWORD", "123456"),
-        database=os.environ.get("DB_NAME", "expense_tracker"),
-        port=int(os.environ.get("DB_PORT", 3306))
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        database=os.environ.get("DB_NAME"),
+        port=int(os.environ.get("DB_PORT")),
+        ssl_ca="ca.pem"
     )
 
 def login_required(route_function):
@@ -336,3 +340,14 @@ def delete_transaction(id):
 
 if __name__ == "__main__":
     app.run(debug=True)
+if __name__ == "__main__":
+ try:
+        db = get_db()
+        print("✅ Aiven MySQL connected successfully!")
+        db.close()
+
+ except Exception as e:
+        print("❌ Aiven MySQL connection failed:")
+        print(e)
+
+ app.run(debug=True)
